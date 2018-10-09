f490631f5cfa9a51778168e7224bf120d03a2950
YunzhengGuo <1286490880@qq.com>
2018-10-09 20:57:49 +0800

    first commit

---
+++ gshop_parent/gshop_common/src/main/java/com/gyz/gshop/App.java
@@ -0,0 +1,13 @@
+package com.gyz.gshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
